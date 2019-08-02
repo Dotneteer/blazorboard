37f491d3ed9d892b4f5482f47c96935b8f9cf6c5
Istvan Novak <dotneteer@hotmail.com>
2019-08-02 08:18:43 +0200

    RadzenComponents added.

---
+++ Dotneteer.BlazorBoard.Components/ComboDataItem.cs
@@ -0,0 +1,24 @@
+﻿namespace Dotneteer.BlazorBoard.Components
+{
+    /// <summary>
+    /// Represents a combo box data item with an id and a display name
+    /// </summary>
+    public class ComboDataItem
+    {
+        public ComboDataItem(string id, string displayName)
+        {
+            Id = id;
+            DisplayName = displayName;
+        }
+
+        /// <summary>
+        /// Item identifier
+        /// </summary>
+        public string Id { get; }
+
+        /// <summary>
+        /// Name to display
+        /// </summary>
+        public string DisplayName { get; }
+    }
+}
