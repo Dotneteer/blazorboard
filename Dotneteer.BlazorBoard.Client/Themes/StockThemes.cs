768865547a3dac9f547dd016b25cae1a5f4602c6
Istvan Novak <dotneteer@hotmail.com>
2019-08-11 09:59:49 +0200

    Components show up in the preview pane

--- Dotneteer.BlazorBoard.Client/Themes/StockThemes.cs
+++ Dotneteer.BlazorBoard.Client/Themes/StockThemes.cs
@@ -10,13 +10,17 @@
         /// </summary>
         public static ThemeProps DarkTheme = new ThemeProps
         {
-            TitleBarBackgroundColor = "#202020",
+            AppBackgroundColor = "#202020",
+            TitleBarBackgroundColor = "transparent",
+            TitleColor = "#f89406",
+            TitleLabelColor = "#f0f0f0",
             FieldBackgroundColor = "#383838",
             FieldBorder = "1px solid #505050",
             FieldColor = "#f0f0f0",
             FieldHoverBackgroundColor = "#094771",
             FieldHiliteBackgroundColor = "#007acc",
             FieldPlaceholderColor = "#b0b0b0",
+            SourcePanelBackgroundColor = "#383838",
 
             EditorTheme = "vs-dark"
         };
@@ -26,13 +30,17 @@
         /// </summary>
         public static ThemeProps LightTheme = new ThemeProps
         {
-            TitleBarBackgroundColor = "#e0e0e0",
+            AppBackgroundColor = "#e0e0e0",
+            TitleBarBackgroundColor = "transparent",
+            TitleColor = "#f89406",
+            TitleLabelColor = "#101010",
             FieldBackgroundColor = "#f0f0f0",
             FieldBorder = "1px solid #d0d0d0",
             FieldColor = "#101010",
             FieldHoverBackgroundColor = "#007acc",
             FieldHiliteBackgroundColor = "#007acc",
             FieldPlaceholderColor = "#808080",
+            SourcePanelBackgroundColor = "#f0f0f0",
 
             EditorTheme = "vs"
         };
--- Dotneteer.BlazorBoard.Client/Themes/ThemeProps.cs
+++ Dotneteer.BlazorBoard.Client/Themes/ThemeProps.cs
@@ -7,13 +7,17 @@ namespace Dotneteer.BlazorBoard.Client.Themes
     /// </summary>
     public class ThemeProps
     {
+        public string AppBackgroundColor { get; set; }
         public string TitleBarBackgroundColor { get; set; }
+        public string TitleColor { get; set; }
+        public string TitleLabelColor { get; set; }
         public string FieldBackgroundColor { get; set; }
         public string FieldBorder { get; set; }
         public string FieldColor { get; set; }
         public string FieldHoverBackgroundColor { get; set; }
         public string FieldHiliteBackgroundColor { get; set; }
         public string FieldPlaceholderColor { get; set; }
+        public string SourcePanelBackgroundColor { get; set; }
 
 
         [NonCss]
